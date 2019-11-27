fa459f6230a02a3801ae2589dbf15247ecafa62b
bong98 <55526360+bong98@users.noreply.github.com>
2019-11-27 13:01:54 +0800

    unittest

---
+++ converted_battleship/UnitTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NUnit.Framework;
+namespace BattleShips
+{
+	[TestFixture ()]
+	public class UnitTest
+	{
+		[Test ()]
+		public void EasyMode ()
+		{
+			GameController.SetDifficulty (AIOption.Easy);
+			GameController.StartGame ();
+			Assert.IsInstanceOf<AIEasyPlayer> (GameController.ComputerPlayer);
+		}
+
+		[Test ()]
+		public void MediumMode ()
+		{
+			GameController.SetDifficulty (AIOption.Medium);
+			GameController.StartGame ();
+			Assert.IsInstanceOf<AIMediumPlayer> (GameController.ComputerPlayer);
+		}
+
+		[Test ()]
+		public void HardMode ()
+		{
+			GameController.SetDifficulty (AIOption.Hard);
+			GameController.StartGame ();
+			Assert.IsInstanceOf<AIHardPlayer> (GameController.ComputerPlayer);
+		}
+	}
+}
